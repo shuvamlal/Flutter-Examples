5162d8f97a6514aed86932ed2c09b814199c947e
Shuvam <shuvamkumar71@gmail.com>
2020-09-07 01:34:21 +0530

    cp2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,36 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome Start"),
       ),
-      body: Text("Have a great start."),
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(10),
+            gradient: LinearGradient(colors: [
+              Colors.pink,
+              Colors.yellow
+            ]),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.pink,
+                blurRadius: 10
+              )
+            ]
+          ),
+          child: Text(
+            "I'm a box.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
